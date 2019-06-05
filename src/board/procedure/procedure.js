d8695f685bc2942b66929c96ce0f206badcb87d0
kismet1216 <lyraemoon@gmail.com>
2019-06-05 11:44:32 -0400

    add http

--- src/board/procedure/procedure.js
+++ src/board/procedure/procedure.js
@@ -1,28 +1,28 @@
 import React from 'react';
 import Card from './card/card';
+import axios from 'axios';
+import { END_POINT } from '../../constants/constants';
 
 export default class Procedure extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      cards: [
-        { title: 'senior developer', dest: 'amz', date: '6/5/2019', resume: 'r1.pdf', contact: { recruiter: 'Wang', phone: '978234223', email: 'ff@gmail.com' } },
-        { title: 'web developer', dest: 'baidu', date: '6/6/2019', resume: 'r5.pdf', contact: { recruiter: 'Li', phone: '1784567894', email: 'gf@gmail.com' } },
-        { title: 'manager', dest: 'fb', date: '6/1/2019', resume: 'r2.pdf', contact: { recruiter: 'Tu', phone: '966234223', email: 'gg@gmail.com' } }
-      ]
+      cards: []
     }
   }
 
   componentDidMount() {
-    // todo call api to get cards
+    axios.get(END_POINT + 'procedure/1').then(cards => {
+      this.setState({cards});
+    });
   }
 
   render() {
     return (
       <div className="container-fluid border rounded">
         {this.state.cards.map(card =>
-          <Card info={card} />
+          <Card info={card} key={card.id} />
         )}
       </div>
     );
---
+++ src/constants/constants.js
@@ -0,0 +1,1 @@
+export const END_POINT = 'http://mock-api.com/WmnE0bgJ.mock/';
