eee2d73dfa0d2f8d8f1d56557f76af3cbdaa3595
Zeshi Zheng <mailtozeshi@gmail.com>
2019-06-24 21:42:22 -0400

    move card saga fix

--- src/redux/saga/move-card.saga.js
+++ src/redux/saga/move-card.saga.js
@@ -4,13 +4,13 @@ import { PROCEDURES_SET } from '../../constants';
 
 export default function* fetchMoveCard({payload}) {
   try {
-    const oldProcedures = yield select((store) => store.procedures);
+    const oldProcedures = yield select((store) => store.proceduresState);
     let npid = payload.npid;
     if (!npid) {
       npid = oldProcedures[oldProcedures.findIndex(p => p.id === payload.opid) + 1].id;
     }
     const procedures = yield call(moveCardApi, payload.cid, npid);
-    yield put(PROCEDURES_SET, procedures);
+    yield put({type: PROCEDURES_SET, payload: procedures});
   } catch {
   }
 }
